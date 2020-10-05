b91d47ef7da9f1a2a81331baf704c00f99015a63
Vikrant Singh <VikrantSingh016>
2020-10-05 22:31:50 +0530

    Updates

---
+++ precache-manifest.42e13dcba96659f213da023a5b0cd9a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5d531da109e6ac87fad62f71c75a66",
+    "url": "/cal/index.html"
+  },
+  {
+    "revision": "5da013385beafb7224fa",
+    "url": "/cal/static/css/main.c710ee8c.chunk.css"
+  },
+  {
+    "revision": "260a504e8e87556455b1",
+    "url": "/cal/static/js/2.c4716f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cal/static/js/2.c4716f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da013385beafb7224fa",
+    "url": "/cal/static/js/main.7e7c93aa.chunk.js"
+  },
+  {
+    "revision": "bd559490bc8c40825acc",
+    "url": "/cal/static/js/runtime-main.4273488b.js"
+  },
+  {
+    "revision": "ee50e50dacbd115032cc28f59c4cbe69",
+    "url": "/cal/static/media/cal.ee50e50d.png"
+  }
+]);
